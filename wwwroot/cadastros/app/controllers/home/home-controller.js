8b986fd928f91f273e34cccdee79a6e0d0b919ea
Alessandro Tulioli <alessandro.smarapd@hotmail.com>
2021-04-21 15:02:39 -0300

    Cria projeto front end com Angular Js

---
+++ wwwroot/cadastros/app/controllers/home/home-controller.js
@@ -0,0 +1,16 @@
+function () {
+    'use strict';
+    angular.module('mwa').controller('HomeCtrl', HomeCtrl);
+
+    HomeCtrl.$inject = [];
+
+    function HomeCtrl() {
+        var vm = this;
+
+        activate();
+
+        function activate() {
+
+        }
+    };
+}
